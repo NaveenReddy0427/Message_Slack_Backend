0b7f2c23afb3f8476c98675c1001b75fb786d955
NaveenReddy0427 <naveendeva909@gmail.com>
2025-05-27 12:47:09 +0530

    completed workspace API layer successfully

--- src/routes/v1/workspaces.js
+++ src/routes/v1/workspaces.js
@@ -1,8 +1,8 @@
 import express from 'express'
 
-import { createWorkspaceController, deleteWorkspaceController, getWorkspaceController, getWorkspacesUserIsMemberOfController } from '../../controllers/workspaceController.js'
+import { addChannelToWorkspaceController, addMemberToWorkspaceController, createWorkspaceController, deleteWorkspaceController, getWorkspaceByJoinCodeController, getWorkspaceController, getWorkspacesUserIsMemberOfController, updateWorkspaceController } from '../../controllers/workspaceController.js'
 import { isAuthenticated } from '../../middlewares/authMiddleware.js'
-import { createWorkspaceSchema } from '../../validators/workspaceSchema.js'
+import { addChannelToWorkspaceSchema, addMemberToWorkspaceSchema, createWorkspaceSchema } from '../../validators/workspaceSchema.js'
 import { validate } from '../../validators/zodValidator.js'
 
 const router = express.Router()
@@ -11,6 +11,10 @@ router.post('/', isAuthenticated, validate(createWorkspaceSchema), createWorkspa
 router.get('/', isAuthenticated, getWorkspacesUserIsMemberOfController)
 router.delete('/:workspaceId', isAuthenticated, deleteWorkspaceController)
 router.get('/:workspaceId', isAuthenticated, getWorkspaceController)
+router.get('/join/:joinCode', isAuthenticated, getWorkspaceByJoinCodeController);
+router.put('/:workspaceId', isAuthenticated, updateWorkspaceController);
+router.put('/:workspaceId/members', isAuthenticated, validate(addMemberToWorkspaceSchema), addMemberToWorkspaceController);
+router.put('/:workspaceId/channels', isAuthenticated, validate(addChannelToWorkspaceSchema), addChannelToWorkspaceController);
 
 
 export default router
