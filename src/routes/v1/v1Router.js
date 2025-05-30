54c2e05730402cba198230d7cfdb16474b8b4647
NaveenReddy0427 <naveendeva909@gmail.com>
2025-05-30 19:11:14 +0530

    add member api to check the subscription of a workspace

--- src/routes/v1/v1Router.js
+++ src/routes/v1/v1Router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import chanelRouter from './channel.js'
+import memberRouter from './members.js'
 import userRouter from './users.js';
 import workspaceRouter from './workspaces.js'
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/users', userRouter);
 router.use('/workspaces', workspaceRouter)
 router.use('/channels', chanelRouter)
+router.use('/members', memberRouter)
 
 export default router;
